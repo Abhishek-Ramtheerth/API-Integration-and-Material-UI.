e93c4fd0566a81102dc6f815b90f272c3e8ee27e
Abhishek-Ramtheerth <AbhishekRamtheerth23@gmail.com>
2024-06-08 22:25:00 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography, Container, TextField, List, ListItem, ListItemText, CircularProgress, Grid } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+
+  // Fetch data from the API on component mount
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  // Update search state on input change
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  // Filter data based on search input
+  const filteredData = data.filter(item => 
+    item.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">React Material-UI App</Typography>
+        </Toolbar>
+      </AppBar>
+      <div className='container'>
+      <Container >
+        <Typography variant="h4" gutterBottom>
+          List 
+        </Typography>
+        <TextField 
+          label="Search" 
+          variant="outlined" 
+          fullWidth 
+          margin="normal"
+          value={search}
+          onChange={handleSearchChange}
+        />
+        {loading ? (
+          <Grid container justifyContent="center">
+            <CircularProgress />
+          </Grid>
+        ) : (
+          <List >
+            {filteredData.map(item => (
+              <ListItem key={item.id} alignItems="flex-start">
+                <ListItemText 
+                  primary={item.title} 
+                  secondary={item.body} 
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </Container>
+      </div>
     </div>
   );
 }
